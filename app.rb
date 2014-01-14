4177c490f9ff26f6273b2afcb8b32fa16ddad953
Rob Heaton <rob@pingpanel.com>
2014-01-14 10:27:31 +0000

    Remove refs to token

--- app.rb
+++ app.rb
@@ -9,13 +9,13 @@ get '/' do
   <br/>
   <a href="http://robertheaton.com">Read my blog post explaining how</a>
 
-  <script type="text/javascript" src="/tracking_token.js"></script>
+  <script type="text/javascript" src="/tracking_id.js"></script>
   <script type="text/javascript">
     document.getElementById("trackingId").innerHTML = trackingId
   </script>'
 end
 
-get '/tracking_token.js' do
+get '/tracking_id.js' do
   cache_control :private
   last_modified Time.at(0)
   response['Content-Type'] = 'text/javascript'
