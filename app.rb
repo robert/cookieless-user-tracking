9650d2c5bebc0a0f86e0859c93086c85ed38dacc
Rob Heaton <rob@pingpanel.com>
2014-01-14 10:26:24 +0000

    remove alert

--- app.rb
+++ app.rb
@@ -3,18 +3,20 @@ require 'rack/cache'
 require 'securerandom'
 
 get '/' do
-  '<script type="text/javascript" src="/tracking_token.js"></script>
-  <script type="text/javascript">
-  alert("Your trackingId is " + trackingId);
-  </script>
+  'Your tracking id is <span id="trackingId"></span>. Try refreshing the page.
   Looks like you\'ve been tagged, even without cookies.
   <br/>
   <br/>
   <a href="http://robertheaton.com">Read my blog post explaining how</a>
-  '
+
+  <script type="text/javascript" src="/tracking_token.js"></script>
+  <script type="text/javascript">
+    document.getElementById("trackingId").innerHTML = trackingId
+  </script>'
 end
 
 get '/tracking_token.js' do
+  cache_control :private
   last_modified Time.at(0)
   response['Content-Type'] = 'text/javascript'
 
